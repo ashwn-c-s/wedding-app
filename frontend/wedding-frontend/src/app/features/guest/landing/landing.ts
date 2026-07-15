0dbef6c5f3c2c3655a3bd7256eded68f94e2bca1
C S, Ashwin <ashwin.cs@outlook.com>
2026-07-15 21:58:21 +0530

    Update landing page with illustration and add strucutre to the elements

--- frontend/wedding-frontend/src/app/features/guest/landing/landing.ts
+++ frontend/wedding-frontend/src/app/features/guest/landing/landing.ts
@@ -1,13 +1,15 @@
 import { Component, inject } from '@angular/core';
+import { DatePipe } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map, catchError, EMPTY } from 'rxjs';
 import { ApiService } from '../../../core/services/api.service';
 import { CountdownTimer } from '../../../shared/countdown-timer/countdown-timer';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  imports: [CountdownTimer],
+  imports: [CountdownTimer, RouterLink, DatePipe],
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
 })
@@ -16,7 +18,7 @@ export class Landing {
   private router = inject(Router);
   private api = inject(ApiService);
 
-  private token = this.route.snapshot.params['token'];
+  public token = this.route.snapshot.params['token'];
   invite = toSignal(
     this.api.getInvite(this.token).pipe(
       catchError(() => {
