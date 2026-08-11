139071948de2f24dc9bb99823dd2f3637f0539e9
C S, Ashwin <ashwin.cs@outlook.com>
2026-08-11 09:49:46 +0530

    update backed url

--- frontend/wedding-frontend/src/environments/environment.prod.ts
+++ frontend/wedding-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'YOUR_BACKEND_PROD_URL',
+  apiBaseUrl: 'https://wedding-app-rouge-seven.vercel.app',
   supabaseUrl: 'https://lmueucizxrzegbnxwafg.supabase.co',
   supabaseAnonKey: 'sb_publishable_WnZaZiY5fVc0_liN-sf26Q_tPPk7OaP'
 };
